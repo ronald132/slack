776866d54f2c5a1bff7825c03d3621f174cc5dc3
ronald132 <ronald132@gmail.com>
2019-12-29 03:50:59 +0800

    upload images, show uploaded image on messsage

--- src/components/Messages/Message.js
+++ src/components/Messages/Message.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Comment } from "semantic-ui-react";
+import { Comment, Image } from "semantic-ui-react";
 import moment from "moment";
 
 const isOwnMessage = (message, user) => {
@@ -10,17 +10,25 @@ const timeFromNow = timestamp => {
   return moment(timestamp).fromNow();
 };
 
-function Message({ timestamp, message, user }) {
+const isImage = message => {
+  return message.hasOwnProperty("image") && !message.hasOwnProperty("content");
+};
+
+const Message = ({ timestamp, message, user }) => {
   return (
     <Comment>
       <Comment.Avatar src={message.user.avatar} />
       <Comment.Content className={isOwnMessage(message, user)}>
         <Comment.Author as="a">{message.user.name}</Comment.Author>
         <Comment.Metadata>{timeFromNow(message.timestamp)}</Comment.Metadata>
-        <Comment.Text>{message.content}</Comment.Text>
+        {isImage(message) ? (
+          <Image src={message.image} className="message__image" />
+        ) : (
+          <Comment.Text>{message.content}</Comment.Text>
+        )}
       </Comment.Content>
     </Comment>
   );
-}
+};
 
 export default Message;
