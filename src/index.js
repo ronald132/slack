fe77c3468e14f61da1e66687d951b49beb243139
ronald132 <ronald132@gmail.com>
2019-12-28 13:02:00 +0800

    Add login feature, redux

--- src/index.js
+++ src/index.js
@@ -3,20 +3,55 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import * as serviceWorker from "./serviceWorker";
 
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route, withRouter } from "react-router-dom";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import "semantic-ui-css/semantic.min.css";
+import firebase from "./firebase";
 
-const Root = () => (
-  <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={App} />
-      <Route path="/login" component={Login} />
-      <Route path="/register" component={Register} />
-    </Switch>
-  </BrowserRouter>
+import * as redux from "redux";
+import { Provider, connect } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+import * as actions from "./actions";
+import Spinner from "./Spinner";
+
+const store = redux.createStore(rootReducer, composeWithDevTools());
+
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.dispatch(actions.setUser(user));
+        this.props.history.push("/");
+      }
+    });
+  }
+  render() {
+    return this.props.isLoading ? (
+      <Spinner />
+    ) : (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  isLoading: state.user.isLoading
+});
+
+const RootWithAuth = withRouter(connect(mapStateToProps)(Root));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <RootWithAuth />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
-
-ReactDOM.render(<Root />, document.getElementById("root"));
 serviceWorker.unregister();
